fad121ebbde95e7ae0ab4f7d15fb7f3dd93e0fe1
adamas-oui <yilinl0409@gmail.com>
2021-08-14 23:46:54 -0400

    connect and upload data to Mongodb

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 
-// //connect to mongoose 
-mongoose.connect("mongodb+srv://yl4148:yl4148new@clustertest.mhx8s.mongodb.net/updateDB", { useNewUrlParser: true }, { useUnifiedTopology: true })
+// // //connect to mongoose 
+// mongoose.connect("mongodb+srv://yl4148:yl4148new@clustertest.mhx8s.mongodb.net/updateDB", { useNewUrlParser: true }, { useUnifiedTopology: true })
 
 //require route 
 app.use("/", require("./routes/updateProfileRoute"));
